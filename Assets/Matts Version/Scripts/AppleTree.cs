19c8c267a677dabff0cd46b6b4ecc4bdb8474117
Matthew Sheehan <cementfactorypublishing@gmail.com>
2021-09-18 22:13:45 -0400

    created Apple and edited object scripts
    Created Apple script to destroy after leaving past the screen

--- Assets/Matts Version/Scripts/AppleTree.cs
+++ Assets/Matts Version/Scripts/AppleTree.cs
@@ -14,9 +14,9 @@ public class AppleTree : MonoBehaviour
     //speed at which tree moves
     public float speed = 1f;
 
-    public float leftAndRightEdge = 10f;
+    public float leftAndRightEdge = 30f;
 
-    public float chanceToChangeDirections = .1f;
+    public float chanceToChangeDirections = .02f;
 
     public float secondsBetweenAppleDrops = 1f;
 
@@ -24,23 +24,44 @@ public class AppleTree : MonoBehaviour
     void Start()
         //dropping apples every second
     {
-       
+        Invoke("DropApple", 2f);                                      
+
     }
+
+
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);      
+
+        apple.transform.position = transform.position;                
+
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         //Basic Movement
         Vector3 pos = transform.position;
-        pos.x = +speed * Time.deltaTime;
+
+        pos.x += speed * Time.deltaTime;
+
         transform.position = pos;
+
         //Changing Direction
         if (pos.x < -leftAndRightEdge)
         {
             speed = Mathf.Abs(speed);// Move right
-        }else if(pos.x > leftAndRightEdge)
+        } else if (pos.x > leftAndRightEdge)
         {
             speed = -Mathf.Abs(speed);// Move left
         }
+    }
+    void FixedUpdate(){
+        if (Random.value < chanceToChangeDirections)
+        { speed *= -1;    // change direction
+         }
     }
 }
